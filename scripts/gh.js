aecc09c79ddb2dfb058c038de8811adff8dc0107
Alberto <alber7rp@gmail.com>
2017-02-17 00:53:02 +0000

    Modificación script push-to-gh y añadida dependencia simple-git

--- scripts/gh.js
+++ scripts/gh.js
@@ -9,6 +9,23 @@ exec("git commit -m 'commit automático'", puts);
 exec("git push origin master", puts);*/
 
 console.log("Commit and push to github master");
-var simpleGit = require('simple-git')();
-simpleGit.add('.')
-         .commit("commit automático").push('origin', 'master');;
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+rl.question('Introduzca el mensaje representativo del commit ', (answer) => {
+  
+  var simpleGit = require('simple-git')();
+	simpleGit.add('.')
+         .commit(answer).push('origin', 'master');
+
+  console.log('commited!');
+  rl.close();
+});
+
+
