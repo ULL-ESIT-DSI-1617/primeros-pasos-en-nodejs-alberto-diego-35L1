0635d01c9ecb3071b2f4ae2b0fd45373712e2941
Alberto <alber7rp@gmail.com>
2017-02-18 01:56:02 +0000

    Modificacion script deploy wiki, añadida dependencia del modulo fs-promise

--- scripts/deploy-wiki.js
+++ scripts/deploy-wiki.js
@@ -1,7 +1,9 @@
 var path = require('path');
 var repo = require('../package.json').repository.wiki;
 var simpleGit = require('simple-git')(path.resolve('wiki'));
+var fsp = require('fs-promise');
 
+	fsp.remove(path.resolve('wiki/.git'));
 
 	simpleGit.init().add('./*')
          .commit('deploy to wiki').addRemote('origin', repo).push('--force','origin', 'master:master');
